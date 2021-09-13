dbb8526431f0b9733ee2dc38ba2426b6a10c1988
yeonsuBeak <soothbugger@gmail.com>
2021-09-13 23:42:10 +0900

    🚧 Import tiny-slider js and implement mobile carousel style

---
+++ js/homeCarousel.js
@@ -0,0 +1,14 @@
+const homeCarousel = tns({
+  container: '.slider-list',
+  slideBy: 'page',
+  autoplay: true,
+  controls: false,
+  navContainer: '.pagination-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
