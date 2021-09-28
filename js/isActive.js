ef69f8f631cb795b1a708a2b9a6f26a5011f3ab0
yeonsuBeak <soothbugger@gmail.com>
2021-09-28 13:33:49 +0900

    🚧 Implement isActive.js for branch-modal

--- js/isActive.js
+++ js/isActive.js
@@ -31,6 +31,20 @@ window.onload = function () {
         .classList.toggle('is-active')
     })
 
+  document
+    .querySelector('.gyeonggi.up-to-9')
+    .addEventListener('click', function () {
+      document.querySelector('.branch-modal').classList.add('is-active')
+      document.querySelector('.overlay').classList.add('is-active')
+    })
+
+  document
+    .querySelector('.branch-modal .ic-close')
+    .addEventListener('click', function () {
+      document.querySelector('.branch-modal').classList.remove('is-active')
+      document.querySelector('.overlay').classList.remove('is-active')
+    })
+
   function productsItem(i) {
     let selectItem = document.querySelectorAll('.products-item')[i]
 
