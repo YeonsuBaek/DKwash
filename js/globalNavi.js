298ab9f99976f9314278ed6355681f112a5cb4d8
yeonsuBeak <soothbugger@gmail.com>
2021-11-06 05:11:32 +0900

    🚧 Apply the hover event for gnb-nav-item

--- js/globalNavi.js
+++ js/globalNavi.js
@@ -14,9 +14,12 @@ document
   .querySelector('.gnb-sub.lg-only')
   .addEventListener('mouseleave', function () {
     document.querySelector('.gnb-sub.lg-only').classList.remove('is-active')
+    document
+      .querySelector('.gnb-nav-item.is-hover')
+      .classList.remove('is-hover')
   })
 
-function gnb(i) {
+function gnbItem(i) {
   document
     .querySelectorAll('.gnb-nav-item')
     [i].addEventListener('mouseenter', function () {
@@ -25,17 +28,23 @@ function gnb(i) {
       for (j = 0; j < 5; j++) {
         document.querySelectorAll('.gnb-sub-list')[j].style.visibility =
           'hidden'
+        document
+          .querySelectorAll('.gnb-nav-item')
+          [j].classList.remove('is-hover')
 
         if (j == i) {
-          document.querySelectorAll('.gnb-sub-list')[i].style.visibility =
+          document.querySelectorAll('.gnb-sub-list')[j].style.visibility =
             'visible'
+          document
+            .querySelectorAll('.gnb-nav-item')
+            [j].classList.add('is-hover')
         }
       }
     })
 }
 
-gnb(0)
-gnb(1)
-gnb(2)
-gnb(3)
-gnb(4)
+gnbItem(0)
+gnbItem(1)
+gnbItem(2)
+gnbItem(3)
+gnbItem(4)
